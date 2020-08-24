21a6732902bae054b9db5409aa57ed1e610fcb66
chirathv97 <66962126+chirathv97@users.noreply.github.com>
2020-08-24 22:37:25 +0530

    update

--- UserJobs.php
+++ UserJobs.php
@@ -8,6 +8,7 @@
   </head>
   <body>
   <?php include_once("inc/Header.php"); ?>
+  <link rel="stylesheet" href="css/newJobs.css">
     <?php 
     require_once 'controller/ui.ctrl.php';
     $loginId='971650834v';//$_SESSION['loginID'];
@@ -15,16 +16,51 @@
     $result=$uictrll-> requestHistory($loginId);
     if ($uictrll->hasJobsSubmitted($result)) {
         ?>
-        <table>
-			<thead>
-                <tr>
-                    <th>Request ID</th>
-                    <th>Request Type</th>
-                    <th>Status</th>
-                    <th>Description</th>
-                </tr>
-			</thead>
-			<tbody>       
+ 
+
+<div class="row">
+<div class="col-sm-8"> </div>
+<div class="col-sm-4"> 
+        <div class="search-bar"><input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names.."></div>
+</div>
+</div>
+
+
+        <div>
+        <div class='table-responsive'>
+ <!--Table-->
+ <table id="tablePreview" class="table table-hover table-striped">
+ <!--Table head-->
+   <thead>
+     <tr>
+       <th>Request #</th>
+       <th>Request ID</th>
+       <th>Request Type</th>
+       <th>User ID</th>
+       <th></th>
+       
+     </tr>
+   </thead>
+   <!--Table head-->
+   <!--Table body-->
+   <tbody>
+     <tr>
+       <th scope="row">1</th>
+       <td>Mark</td>
+       <td>Otto</td>
+       <td>@mdo</td>
+       <td><button type="button" class="btn btn-success">Proceed</button></td>       
+     </tr>
+     
+     
+   </tbody>
+   <!--Table body-->
+ </table>
+ <!--Table-->
+</div>
+        </div>
+    </div>
+<?php include_once('inc/Footer.php'); ?>      
     <?php
     while ($row=$uictrll->fetchData($result)){
         echo "<tr><td class='column1' >" .$row["requestNo"]."</td><td class='column1'>".$row["details"]."</td><td class='column1'>".$row["status"] . "</td><td class='column1'>".$row["description"]."</td></tr>";
