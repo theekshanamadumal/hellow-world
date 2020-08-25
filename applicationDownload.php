96ce22c1710c3df1333c2e67039c7b663f3172ee
unknown <theekshana.18@cse.mrt.ac.lk>
2020-08-25 23:54:27 +0530

    appilcatio forms

--- applicationDownload.php
+++ applicationDownload.php
@@ -5,12 +5,53 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>download applications</title>
 </head>
+
+
+
 <body>
 <?php include_once("inc/Header.php"); ?>
+<div class="container" >
+<div class="row">
+    <div class="col-sm-3">
+        <br><br>You can download any application form out of given forms as a word document by clicking on the word icon or application form name.<br><br> Then save the word file by clicking save buttton on imerging window.
+    </div>
+    <div class="col-sm-9">
 
 
+<ul style="list-style: none;">
+
+<?php $icon='<img src="img/msWord.png" alt="word File" width="10%"><span class="container p-3 my-3 bg-formcolor text-white applwidth">';?>
+<br><br>
+
+<li><a href="files/applications/common35.docx"> <?php echo $icon;?> common 035 </a></span></li>
+<li><a href="files/applications/common86.docx"> <?php echo $icon;?> common 086 </a></span></li>
+<li><a href="files/applications/common126.docx"> <?php echo $icon;?> common 126 </a></span></li>
+<li><a href="files/applications/common147.docx"> <?php echo $icon;?> common 147 </a></span></li>
+<li><a href="files/applications/common148.docx"> <?php echo $icon;?> common 148 </a></span></li>
+<li><a href="files/applications/අග්‍රහාර ඉල්ලීම් නිර්දේශ කිරීම.docx"> <?php echo $icon;?> අග්‍රහාර ඉල්ලීම් නිර්දේශ කිරීම </a></span></li>
+<li><a href="files/applications/ගුරු සේවයේ උසස්වීම් නිර්දේශ කිරීම.docx"> <?php echo $icon;?> ගුරු සේවයේ උසස්වීම් නිර්දේශ කිරීම </a></span></li>
+<li><a href="files/applications/ගුරු සේවා කාලය ඒකාබද්ධ කිරීම.docx"> <?php echo $icon;?> ගුරු සේවා කාලය ඒකාබද්ධ කිරීම </a></span></li>
+<li><a href="files/applications/චාරිකා.docx"> <?php echo $icon;?> චාරිකා </a></span></li>
+<li><a href="files/applications/නිවාඩු.docx"> <?php echo $icon;?> නිවාඩු </a></span></li>
+<li><a href="files/applications/පශ්චාත් උපාධි අධ්‍යාපන ඩිප්ලෝමා පාඨමාලා ගාස්තුවෙන් අඩක් ආපසු ගෙවීම.docx"> <?php echo $icon;?> පශ්චාත් උපාධි අධ්‍යාපන ඩිප්ලෝමා පාඨමාලා ගාස්තුවෙන් අඩක් ආපසු ගෙවීම </a></span></li>
+<li><a href="files/applications/පාසල් ළමුන්ට දිවා ආහාර සදහා වූ පෝෂණ වවුචර්.docx"> <?php echo $icon;?> පාසල් ළමුන්ට දිවා ආහාර සදහා වූ පෝෂණ වවුචර් </a></span></li>
+<li><a href="files/applications/පාසැල් ගොඩනැගිලි අවශ්‍යතා නිර්දේශ කර අනුමැතිය ඉදිරිපත් කිරීම.docx"> <?php echo $icon;?> පාසැල් ගොඩනැගිලි අවශ්‍යතා නිර්දේශ කර අනුමැතිය ඉදිරිපත් කිරීම </a></span></li>
+<li><a href="files/applications/පාසැල් ගොඩනැගිලි, ක්‍රීඩාපිටි ප්‍රජා අවශ්‍යතා සදහා ලබාදීම.docx"> <?php echo $icon;?> පාසැල් ගොඩනැගිලි, ක්‍රීඩාපිටි ප්‍රජා අවශ්‍යතා සදහා ලබාදීම </a></span></li>
+<li><a href="files/applications/වැටුප් ණය ගෙවීම.docx"> <?php echo $icon;?> වැටුප් ණය ගෙවීම </a></span></li>
+<li><a href="files/applications/වැටුප් වර්ධක අනුමත කිරීම.docx"> <?php echo $icon;?> වැටුප් වර්ධක අනුමත කිරීම </a></span></li>
+<li><a href="files/applications/වැටුප් වර්ධක අනුමත කිරීම.docx"> <?php echo $icon;?> වැටුප් වර්ධක අනුමත කිරීම </a></span></li>
+<li><a href="files/applications/වැන්දඹු හා අනත් දරු විශ්‍රාම වැටුප් අංක ලබාගැනීම.docx"> <?php echo $icon;?> වැන්දඹු හා අනත් දරු විශ්‍රාම වැටුප් අංක ලබාගැනීම </a></span></li>
+<li><a href="files/applications/විදේශ නිවාඩු.docx"> <?php echo $icon;?> විදේශ නිවාඩු </a></span></li>
+<li><a href="files/applications/විශ්‍රාම පුර්ව නිවාඩු.docx"> <?php echo $icon;?> විශ්‍රාම පුර්ව නිවාඩු </a></span></li>
+<li><a href="files/applications/ශිෂ්‍ය කාර්ය දර්ශනයේ දෙවැනි පිටපත සදහා අනුමැතිය ලබාදීම.docx"> <?php echo $icon;?> ශිෂ්‍ය කාර්ය දර්ශනයේ දෙවැනි පිටපත සදහා අනුමැතිය ලබාදීම </a></span></li>
+<li><a href="files/applications/සාමාන්‍ය වැටුප් විස්තරයක් ලබාගැනීම.docx"> <?php echo $icon;?> සාමාන්‍ය වැටුප් විස්තරයක් ලබාගැනීම </a></span></li>
+<li><a href="files/applications/සේවයෙන් විශ්‍රාම ගැන්වීම නිර්දේශ කිරීම.docx"> <?php echo $icon;?> සේවයෙන් විශ්‍රාම ගැන්වීම නිර්දේශ කිරීම </a></span></li>
 
 
+</ul>
+</div>
+</div>
+</div>
 
 
 <?php include_once('inc/Footer.php'); ?>
