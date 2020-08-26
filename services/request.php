1f3a72d917436ebf840bb721b04f60051f4e512b
unknown <theekshana.18@cse.mrt.ac.lk>
2020-08-26 14:18:26 +0530

    serv

--- services/request.php
+++ services/request.php
@@ -2,7 +2,7 @@
 
     
 
-    require_once '../controller/request.ctrl.php';
+    require_once 'controller/request.ctrl.php';
     $reqctrll=new requestCtrl();
     $massege=$reqctrll->requst($variable);
     echo$massege;
