698d5c3ecf0780db73ddd1d2bf16f8d61aa9f733
unknown <theekshana.18@cse.mrt.ac.lk>
2020-08-26 16:22:53 +0530

    file u

--- adminLogin.php
+++ adminLogin.php
@@ -1,7 +1,7 @@
 <html>
 <head><title> Login page</title></head>
 <body>
-
+<?php include_once("inc/Header.php"); ?>
 <?php
 if   (isset($_POST['submit'])){
     $username=$_POST['user_name'];
@@ -21,7 +21,7 @@ if   (isset($_POST['submit'])){
 
 ?>
 
-<?php include_once("inc/Header.php"); ?>
+
 <div class="row">
 
     <div class = "col"></div>
