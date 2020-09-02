e70e34d6a8de6ef1670939886eab2ec9eee6e71b
unknown <theekshana.18@cse.mrt.ac.lk>
2020-09-02 18:46:59 +0530

    userfileupload

--- controller/fileupload.ctrl.php
+++ controller/fileupload.ctrl.php
@@ -1,12 +1,28 @@
 <?php
 include_once 'model/fileupload.model.php';
 class fileuploader{
+
 	public function uploadFile($file,$category,$NIC,$description)
     {
 		$targetFileName=$NIC.'-'.$description;
-	    $target_file = "files/records/".$category."/".$targetFileName;
-	           
+		$target_file = "files/records/".$category."/".$targetFileName;
 
+		list($message,$status)=$this->saveFile($file,$target_file);
+
+		if ($status=="success") {
+			$tableUpdate=$this->updateRecordTable($category,$description,$NIC);
+				if (!$tableUpdate) {
+					$message =  "The file table uploade error ";
+					$status="error";
+				}
+		}	
+		return array($message,$status);
+	}
+
+
+	public function saveFile($file,$target_file)
+	{
+		
 		if(!$this->is_file_choosen($file)){
 			$message = "No file selected";
 			$status="warning";
@@ -23,27 +39,27 @@ class fileuploader{
 		    }
 
 		    else{
-		      if (move_uploaded_file($file["tmp_name"], $target_file)) {
-				$tableUpdate=$this->updateRecordTable($category,$description,$NIC);
-				if ($tableUpdate) {
+
+		      	if (move_uploaded_file($file["tmp_name"], $target_file)) {
+				
 					$message =  "The file ". basename( $file["name"]). " has been uploaded Successfully.";
 					$status="success";
 				}
-				else {
-					$message="table update failed";
+				
+			  
+			  	else {
+					$message = "Sorry, there was an error uploading your file.";
 					$status="error";
 				}
-
-		      	
-		      } else {
-				$message = "Sorry, there was an error uploading your file.";
-				$status="error";
-		      }
+			
 		    }
 		}
 		return array($message,$status);
-
 	}
+
+
+
+
 	public function is_file_choosen()
     {
         if(!isset($file) || $file['error'] == UPLOAD_ERR_NO_FILE) {
@@ -63,6 +79,69 @@ class fileuploader{
 	}
 	
 	
+}
+
+
+
+class userJobFileUploader{
+
+	public function saveFiles($fileList,$filePath)
+	{ 
+		if (!file_exists($filePath)){
+			mkdir($filePath);
+		}
+		$fileCount=0;
+		foreach ($fileList['name'] as $fileName){
+					
+			$size=$fileList['size'][$fileCount];
+			$tempName=$fileList["tmp_name"][$fileCount];
+			$target_file=$filePath.'/'.$fileName;
+			list($message,$status)=$this->uploadFile($target_file,$size,$tempName,$fileName);
+			echo $status.'<br>';
+        
+
+
+
+			echo $fileList['name'][$fileCount].'<br>';
+			$fileCount++;
+
+		}
+
+       
+   
+	}
+
+	public function uploadFile($target_file,$size,$tempName,$fileName)
+	{
+		if (file_exists($target_file)) {
+			$message = "Sorry, file already exists.";
+			$status="warning";
+		  }
+
+		  elseif ( $size> 50000000) {
+			$message = "Sorry, your file is too large.";
+			$status="warning";
+		  }
+
+		else{
+
+			if (move_uploaded_file($tempName, $target_file)) {
+			  
+				$message =  "The file ". basename( $fileName). " has been uploaded Successfully.";
+				$status="success";
+				
+			}
+			  
+			
+			else {
+				$message = "Sorry, there was an error uploading your file.";
+				$status="error";
+			}
+		  
+		}
+		return array($message,$status);
+	}
+
 }
 
 ?>
--- jobFileUpload.view.php
+++ jobFileUpload.view.php
@@ -1,9 +1,27 @@
 
  <!-- Popup form to upload the required attachments for the request -->
+ <?php
+ if(isset($_POST['upload'])){
+    $jobNo=2;
+    $filePath='files/jobFiles/'.$jobNo;
+       
+    include_once"controller/fileupload.ctrl.php";
+    $fileSaver = new userJobFileUploader();
+    $fileSaver->saveFiles($_FILES['fileToUpload'],$filePath);
+
+    
+ }
+ 
+ 
+?>
+ 
+ 
+ 
+ 
  <div class="popup">
   <div class="popup-content">
-  <img src="img/close.jpg" alt="" class="close"> 
-        <form class="popup-form" method='post'>
+  <img src="img/close.jpg" alt="" class="closebtn" > 
+        <form class="popup-form" method='post' multiple="multiple" enctype="multipart/form-data">
             
           <div class="container-fluid">
               <div style="display:inline"><p>Job Type : </p> <p id="jobType"></p>
@@ -11,12 +29,14 @@
               <div>
               <p>Upload required Attachments</p>
               <label for="myfile">Select files:</label>
-              <input type="file" id="myfile" name="myfile" multiple>
+              <input type="file" id="fileToUpload[]" name="fileToUpload[]" multiple>
               </div>
               <br><br>
 
               <!--  -->
               <input type="hidden" name="requestNo" id="hidden" value=''>
+              <input type='submit' name='upload' class='Confirm-btn btn btn-success' id="upload" value='upload' /> 
+              <br><br>
               <input type='submit' name='Confirm' class='Confirm-btn btn btn-success' id="confirm" value='Confirm' /> 
 
           </div>                 
@@ -37,7 +57,7 @@
     
     }    
 
-    document.querySelector(".close").addEventListener("click",function(){
+    document.querySelector(".closebtn").addEventListener("click",function(){
     document.querySelector(".popup").style.display="none";
     })
   </script>
