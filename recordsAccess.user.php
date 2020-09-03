79d36ec98483ef378b2ff77ae6185457f151a701
unknown <theekshana.18@cse.mrt.ac.lk>
2020-09-03 13:11:28 +0530

    rec acc

--- recordsAccess.user.php
+++ recordsAccess.user.php
@@ -1,44 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Access files</title>
+    <title>records</title>
 </head>
-<body>
-    <h1>code to access files</h1>
 
-    <?php 
+<body>
+<?php require_once'inc/Header.php';?>
+<link rel="stylesheet" href="css/jobService.css">
+<link rel="stylesheet" href="css/jobService.css">
+<?php
     if(isset($_SESSION['loginID'])) {
 
         echo'logeed';
 
     }
-    else{?>
-        
- <div class="popup">
-  <div class="popup-content">
+    else{
+        $function='popupFunction();';    
+    
+    
+    }
+?>
+<?php include_once('inc/Footer.php'); ?>
+
+<!-- Popup form to upload the required attachments for the request -->
+
+<div class="popup">
+  <div class="popup-content" >
   <img src="files/img/close.jpg" alt="" class="closebtn" > 
-        <form class="popup-form" method='post' multiple="multiple" enctype="multipart/form-data">
-          <div class="container-fluid">
-              <div style="display:inline"><p>Job Type : <?php echo 'jobtype';?> </p> 
-              </div>
-              <div style="display:inline"><p>Job Name : <?php echo 'jobName';?></p> 
-              </div>
-              <div>
-              <p>Upload required Attachments</p>
-              <label for="myfile">Select files:</label>
-              <input type="file" id="fileToUpload[]" name="fileToUpload[]" multiple>
-              </div>
-              <br><br>
-              <input type="hidden" name="requestNo" id="hidden" value=''>
-              <input type='submit' name='upload' class='Confirm-btn btn btn-success' id="upload" value='Confirm' /> 
-              <br><br>
-          </div>                
-        </form>
+  <div style="padding:7em;">
+  <?php  
+   
+    $message='login before search  records<br>
+        <a href="userLogin.php">login here</a>';
+    echo $message;
+  ?>
   </div>
   </div>
-    <?php }    ?>
+  </div>
+
+
+<!-- Script function to enable popup container-->
+<script>
+    function popupFunction(){  
+    document.querySelector(".popup").style.display="flex";
+    }    
+    document.querySelector(".closebtn").addEventListener("click",function(){
+    document.querySelector(".popup").style.display="none";
+    })
+    <?php echo $function ?>
+  </script>
+
+
 
 </body>
 </html>
