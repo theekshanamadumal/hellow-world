48184ed4adfa187cf244479e6b362fe69f7e55fa
chirathv97 <66962126+chirathv97@users.noreply.github.com>
2020-09-04 00:34:19 +0530

    update

--- jobFiles.view.php
+++ jobFiles.view.php
@@ -11,12 +11,18 @@ $dir = "files/jobFiles/".$jobNo.'/';
 // Open a directory, and read its contents
 if (is_dir($dir)){
   if ($dh = opendir($dir)){
+
+    $file = readdir($dh);
+    echo "     filename:<a href=   '$dir$file'   > $dir$file</a><br>        ";
+
+/*
     while (($file = readdir($dh)) !== false){
 
       echo "     filename:<a href=   '$dir$file'   > $dir$file</a><br>        ";
 
       
     }
+    */
     closedir($dh);
   }
 }
