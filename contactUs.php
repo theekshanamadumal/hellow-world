3d8ced1f9c10355345bcd4cdb368845f7ac6f3db
chirathv97 <66962126+chirathv97@users.noreply.github.com>
2020-09-04 13:51:22 +0530

    update

--- contactUs.php
+++ contactUs.php
@@ -7,44 +7,51 @@
 </head>
 <body>
         <?php include_once("inc/Header.php"); ?>
-        <?php include_once("inc/ArticalHead.php"); ?>
+        <link rel="stylesheet" href="css/home.css">
+       
 
 <div class="container" >
-<div class="row">
+        <div class="row">
 
-<div class="col-sm-6">
+                <div class="col-sm-6">
+                                        
+                <section class="maps" style="margin-top:15%">
+
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7906.782404527497!2d80.1187439974222!3d7.
+                7482648591839425!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3afcd5e7bfa3d463%3A0xd7c55bf1ee61faaa!2
+                sZonal%20Education%20Office!5e0!3m2!1sen!2slk!4v1589674631696!5m2!1sen!2slk"
+                width="100%" height="500em" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0">
+                </iframe> 
+
+                </section>
+                </div>
+
+                <div class="col-sm-6">
                         
-<section class="maps">
-
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7906.782404527497!2d80.1187439974222!3d7.
-7482648591839425!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3afcd5e7bfa3d463%3A0xd7c55bf1ee61faaa!2
-sZonal%20Education%20Office!5e0!3m2!1sen!2slk!4v1589674631696!5m2!1sen!2slk"
- width="100%" height="500em" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0">
- </iframe> 
-
-</section>
-</div>
-</div>
-</div>
-</div>  
-
-
-
-<main>
-   <pre id="container-fluid text-left">
-   
-                 <h2><center>
-               
-Zonal Education Office,
-Nikaweratiya.
-
-Telephone |  0372260214
-Fax       |  0372260354
-
-Email :
+                        <div class="container " style="margin-top:15%; margin-bottom:15%; padding-top:20%">
+                        
+                        
+                        <h3><center>
+                                
+<div>Zonal Education Office,</div>
+<div>Nikaweratiya<br></div>
+<div>Telephone |  0372260214<br></div>
+<div>Fax       |  03722603<br></div>
+<div>Email :
 <a href="http://www.nikaeduzone@gmail.com">nikaeduzone@gmail.com</a>
-                </center></h2>
-                </pre></main>
+</div>                                      </center></h3>
+                        
+                        </div>
+                        
+
+
+                </div>
+        </div>
+</div>
+ 
+
+
+
 
   
 
