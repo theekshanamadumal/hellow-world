553e819c0d0584ac172c33fd5764745bc30cd40b
chirathv97 <66962126+chirathv97@users.noreply.github.com>
2020-09-25 22:44:15 +0530

    Application Download

--- applicationDownload.php
+++ applicationDownload.php
@@ -3,13 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>download applications</title>
+    <title>Download Applications</title>
 </head>
 
 
 
 <body>
 <?php include_once("inc/Header.php"); ?>
+<link rel="stylesheet" href="css/applicationDownload.css">
 <div class="container" >
 <div class="row">
     <div class="col-sm-3" style="font-size: 1.5em;">
@@ -20,32 +21,32 @@
 
 <ul style="list-style: none;">
 
-<?php $icon='<img src="files/img/msWord.png" alt="word File" width="10%"><span class="container p-3 my-3 bg-formcolor text-white applwidth">';?>
+<?php $icon='<div class="link-container"><div class="img-container"><img src="files/img/msWord.png" alt="word File" ></div><div class="text-container">';?>
 <br><br>
 
-<li><a href="files/applications/common35.docx"> <?php echo $icon;?> common 035 </a></span></li>
-<li><a href="files/applications/common86.docx"> <?php echo $icon;?> common 086 </a></span></li>
-<li><a href="files/applications/common126.docx"> <?php echo $icon;?> common 126 </a></span></li>
-<li><a href="files/applications/common147.docx"> <?php echo $icon;?> common 147 </a></span></li>
-<li><a href="files/applications/common148.docx"> <?php echo $icon;?> common 148 </a></span></li>
-<li><a href="files/applications/අග්‍රහාර ඉල්ලීම් නිර්දේශ කිරීම.docx"> <?php echo $icon;?> අග්‍රහාර ඉල්ලීම් නිර්දේශ කිරීම </a></span></li>
-<li><a href="files/applications/ගුරු සේවයේ උසස්වීම් නිර්දේශ කිරීම.docx"> <?php echo $icon;?> ගුරු සේවයේ උසස්වීම් නිර්දේශ කිරීම </a></span></li>
-<li><a href="files/applications/ගුරු සේවා කාලය ඒකාබද්ධ කිරීම.docx"> <?php echo $icon;?> ගුරු සේවා කාලය ඒකාබද්ධ කිරීම </a></span></li>
-<li><a href="files/applications/චාරිකා.docx"> <?php echo $icon;?> චාරිකා </a></span></li>
-<li><a href="files/applications/නිවාඩු.docx"> <?php echo $icon;?> නිවාඩු </a></span></li>
-<li><a href="files/applications/පශ්චාත් උපාධි අධ්‍යාපන ඩිප්ලෝමා පාඨමාලා ගාස්තුවෙන් අඩක් ආපසු ගෙවීම.docx"> <?php echo $icon;?> පශ්චාත් උපාධි අධ්‍යාපන ඩිප්ලෝමා පාඨමාලා ගාස්තුවෙන් අඩක් ආපසු ගෙවීම </a></span></li>
-<li><a href="files/applications/පාසල් ළමුන්ට දිවා ආහාර සදහා වූ පෝෂණ වවුචර්.docx"> <?php echo $icon;?> පාසල් ළමුන්ට දිවා ආහාර සදහා වූ පෝෂණ වවුචර් </a></span></li>
-<li><a href="files/applications/පාසැල් ගොඩනැගිලි අවශ්‍යතා නිර්දේශ කර අනුමැතිය ඉදිරිපත් කිරීම.docx"> <?php echo $icon;?> පාසැල් ගොඩනැගිලි අවශ්‍යතා නිර්දේශ කර අනුමැතිය ඉදිරිපත් කිරීම </a></span></li>
-<li><a href="files/applications/පාසැල් ගොඩනැගිලි, ක්‍රීඩාපිටි ප්‍රජා අවශ්‍යතා සදහා ලබාදීම.docx"> <?php echo $icon;?> පාසැල් ගොඩනැගිලි, ක්‍රීඩාපිටි ප්‍රජා අවශ්‍යතා සදහා ලබාදීම </a></span></li>
-<li><a href="files/applications/වැටුප් ණය ගෙවීම.docx"> <?php echo $icon;?> වැටුප් ණය ගෙවීම </a></span></li>
-<li><a href="files/applications/වැටුප් වර්ධක අනුමත කිරීම.docx"> <?php echo $icon;?> වැටුප් වර්ධක අනුමත කිරීම </a></span></li>
-<li><a href="files/applications/වැටුප් වර්ධක අනුමත කිරීම.docx"> <?php echo $icon;?> වැටුප් වර්ධක අනුමත කිරීම </a></span></li>
-<li><a href="files/applications/වැන්දඹු හා අනත් දරු විශ්‍රාම වැටුප් අංක ලබාගැනීම.docx"> <?php echo $icon;?> වැන්දඹු හා අනත් දරු විශ්‍රාම වැටුප් අංක ලබාගැනීම </a></span></li>
-<li><a href="files/applications/විදේශ නිවාඩු.docx"> <?php echo $icon;?> විදේශ නිවාඩු </a></span></li>
-<li><a href="files/applications/විශ්‍රාම පුර්ව නිවාඩු.docx"> <?php echo $icon;?> විශ්‍රාම පුර්ව නිවාඩු </a></span></li>
-<li><a href="files/applications/ශිෂ්‍ය කාර්ය දර්ශනයේ දෙවැනි පිටපත සදහා අනුමැතිය ලබාදීම.docx"> <?php echo $icon;?> ශිෂ්‍ය කාර්ය දර්ශනයේ දෙවැනි පිටපත සදහා අනුමැතිය ලබාදීම </a></span></li>
-<li><a href="files/applications/සාමාන්‍ය වැටුප් විස්තරයක් ලබාගැනීම.docx"> <?php echo $icon;?> සාමාන්‍ය වැටුප් විස්තරයක් ලබාගැනීම </a></span></li>
-<li><a href="files/applications/සේවයෙන් විශ්‍රාම ගැන්වීම නිර්දේශ කිරීම.docx"> <?php echo $icon;?> සේවයෙන් විශ්‍රාම ගැන්වීම නිර්දේශ කිරීම </a></span></li>
+<li><?php echo $icon;?><a href="files/applications/common35.docx" class="glow">  common 035 </a></div></div></li>
+<li><?php echo $icon;?><a href="files/applications/common86.docx"class="glow">  common 086 </a></div></div></li>
+<li><?php echo $icon;?><a href="files/applications/common126.docx" class="glow">  common 126 </a></div></div></li>
+<li><?php echo $icon;?><a href="files/applications/common147.docx" class="glow">  common 147 </a></div></div></li>
+<li><?php echo $icon;?><a href="files/applications/common148.docx" class="glow">  common 148 </a></div></div></li>
+<li><?php echo $icon;?><a href="files/applications/අග්‍රහාර ඉල්ලීම් නිර්දේශ කිරීම.docx" class="glow">  අග්‍රහාර ඉල්ලීම් නිර්දේශ කිරීම </a></div></div></li>
+<li><?php echo $icon;?><a href="files/applications/ගුරු සේවයේ උසස්වීම් නිර්දේශ කිරීම.docx" class="glow">  ගුරු සේවයේ උසස්වීම් නිර්දේශ කිරීම </a></div></div></li>
+<li><?php echo $icon;?><a href="files/applications/ගුරු සේවා කාලය ඒකාබද්ධ කිරීම.docx" class="glow"> ගුරු සේවා කාලය ඒකාබද්ධ කිරීම </a></div></div></li>
+<li><?php echo $icon;?><a href="files/applications/චාරිකා.docx" class="glow">  චාරිකා </a></div></div></li>
+<li><?php echo $icon;?><a href="files/applications/නිවාඩු.docx" class="glow">  නිවාඩු </a></div></div></li>
+<li><?php echo $icon;?><a href="files/applications/පශ්චාත් උපාධි අධ්‍යාපන ඩිප්ලෝමා පාඨමාලා ගාස්තුවෙන් අඩක් ආපසු ගෙවීම.docx" class="glow"> පශ්චාත් උපාධි අධ්‍යාපන ඩිප්ලෝමා පාඨමාලා ගාස්තුවෙන් අඩක් ආපසු ගෙවීම </a></div></div></li>
+<li><?php echo $icon;?><a href="files/applications/පාසල් ළමුන්ට දිවා ආහාර සදහා වූ පෝෂණ වවුචර්.docx" class="glow">  පාසල් ළමුන්ට දිවා ආහාර සදහා වූ පෝෂණ වවුචර් </a></div></div></li>
+<li><?php echo $icon;?><a href="files/applications/පාසැල් ගොඩනැගිලි අවශ්‍යතා නිර්දේශ කර අනුමැතිය ඉදිරිපත් කිරීම.docx" class="glow">   පාසැල් ගොඩනැගිලි අවශ්‍යතා නිර්දේශ කර අනුමැතිය ඉදිරිපත් කිරීම </a></div></div></li>
+<li><?php echo $icon;?><a href="files/applications/පාසැල් ගොඩනැගිලි, ක්‍රීඩාපිටි ප්‍රජා අවශ්‍යතා සදහා ලබාදීම.docx" class="glow">පාසැල් ගොඩනැගිලි, ක්‍රීඩාපිටි ප්‍රජා අවශ්‍යතා සදහා ලබාදීම </a></div></div></li>
+<li><?php echo $icon;?><a href="files/applications/වැටුප් ණය ගෙවීම.docx" class="glow">  වැටුප් ණය ගෙවීම </a></div></div></li>
+<li><?php echo $icon;?><a href="files/applications/වැටුප් වර්ධක අනුමත කිරීම.docx" class="glow">  වැටුප් වර්ධක අනුමත කිරීම </a></div></div></li>
+<li><?php echo $icon;?><a href="files/applications/වැටුප් වර්ධක අනුමත කිරීම.docx" class="glow">  වැටුප් වර්ධක අනුමත කිරීම </a></div></div></li>
+<li><?php echo $icon;?><a href="files/applications/වැන්දඹු හා අනත් දරු විශ්‍රාම වැටුප් අංක ලබාගැනීම.docx" class="glow">  වැන්දඹු හා අනත් දරු විශ්‍රාම වැටුප් අංක ලබාගැනීම </a></div></div></li>
+<li><?php echo $icon;?><a href="files/applications/විදේශ නිවාඩු.docx" class="glow"> විදේශ නිවාඩු </a></div></div></li>
+<li><?php echo $icon;?><a href="files/applications/විශ්‍රාම පුර්ව නිවාඩු.docx" class="glow"> විශ්‍රාම පුර්ව නිවාඩු </a></div></div></li>
+<li><?php echo $icon;?><a href="files/applications/ශිෂ්‍ය කාර්ය දර්ශනයේ දෙවැනි පිටපත සදහා අනුමැතිය ලබාදීම.docx" class="glow"> ශිෂ්‍ය කාර්ය දර්ශනයේ දෙවැනි පිටපත සදහා අනුමැතිය ලබාදීම </a></div></div></li>
+<li><?php echo $icon;?><a href="files/applications/සාමාන්‍ය වැටුප් විස්තරයක් ලබාගැනීම.docx" class="glow">  සාමාන්‍ය වැටුප් විස්තරයක් ලබාගැනීම </a></div></div></li>
+<li><?php echo $icon;?><a href="files/applications/සේවයෙන් විශ්‍රාම ගැන්වීම නිර්දේශ කිරීම.docx" class="glow"> සේවයෙන් විශ්‍රාම ගැන්වීම නිර්දේශ කිරීම </a></div></div></li>
 
 
 </ul>
